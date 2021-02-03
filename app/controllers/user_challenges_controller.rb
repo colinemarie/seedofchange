6b9bc1ff4240cf762bddf46de1e5bb4a04d4f281
Coline Juillerat <coline.ju@gmail.com>
2021-02-03 20:46:12 +0100

    show user challenges created

--- app/controllers/user_challenges_controller.rb
+++ app/controllers/user_challenges_controller.rb
@@ -1,21 +1,52 @@
 class UserChallengesController < ApplicationController
   def index
-    @user_challenges = UserChallenge.all.where({ user: current_user, status: ["accepted", "pending"] })
-  end
-
-  def new
+    @user_challenges = UserChallenge.all.where({ user: current_user, status: ["accepted"] })
   end
 
   def create
     ##ajouter un while pour un max de 5
-    @challenges = Challenge.all
+    @challenges_id = UserChallenge.all.where(user: current_user).map(&:challenge_id)
+    @challenges = Challenge.all.where.not(id: @challenges_id)
     @challenge = @challenges.sample
     @user_challenge = UserChallenge.new
     @user_challenge.user = current_user
     @user_challenge.challenge = @challenge
     @user_challenge.status = "pending"
     @user_challenge.save
-    redirect_to user_challenges_path
+    redirect_to user_challenge_path(@user_challenge)
+  end
+
+  def show
+    @user_challenge = UserChallenge.find(params[:id])
+    @user_challenges_ongoing = UserChallenge.all.where({ user: current_user, status: ["accepted"] }).count
+  end
+
+  def accept
+    @user_challenge = UserChallenge.find(params[:id])
+    @user_challenge.update(status: "accepted")
+    @user_challenges_ongoing = UserChallenge.all.where({ user: current_user, status: ["accepted"] }).count
+    if @user_challenges_ongoing >= 5
+      render "user_challenges/end"
+    else
+      redirect_to user_challenges_path
+    end
+  end
+
+  def decline
+    @user_challenge = UserChallenge.find(params[:id])
+    @user_challenge.update(status: "declined")
+    @user_challenges_ongoing = UserChallenge.all.where({ user: current_user, status: ["pending", "accepted"] }).count
+    if @user_challenges_ongoing >= 5
+      render "user_challenges/end"
+    else
+      redirect_to user_challenges_path
+    end
+  end
+
+  def drop
+  end
+
+  def validate
   end
 
 end
