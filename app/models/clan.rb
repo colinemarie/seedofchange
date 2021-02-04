d2a6a816385d35e0839da3ca57accd4367440b25
Coline Juillerat <coline.ju@gmail.com>
2021-02-04 20:13:18 +0100

    show debug

--- app/models/clan.rb
+++ app/models/clan.rb
@@ -1,4 +1,3 @@
 class Clan < ApplicationRecord
-  has_many :users
   validates_presence_of :name
 end
--- db/migrate/20210204142402_change_column_default_to_user_challenges_table.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnDefaultToUserChallengesTable < ActiveRecord::Migration[6.0]
-  def change
-    change_column_default :user_challenges, :status, 'pending'
-  end
-end
