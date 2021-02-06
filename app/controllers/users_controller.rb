f0d44dbf80fca9a183d51e65ae7556e060348e57
Myriam Faucher Pelletier <myriamfpelletier@gmail.com>
2021-02-06 18:01:19 +0100

    profilepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @validated_challenges = Challenge.includes(user_challenges: :user).where(user_challenges: {user: current_user, status: "validated"})
+    @category_count = @validated_challenges.group(:category).count
   end
 end
