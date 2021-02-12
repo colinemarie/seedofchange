a6074735d6eb19dc04a888ea1472d3c1d6471586
Simon Dautun <dautun.simon@gmail.com>
2021-02-12 17:05:25 +0100

    Sum the total score of clan

--- app/controllers/clans_controller.rb
+++ app/controllers/clans_controller.rb
@@ -2,5 +2,6 @@ class ClansController < ApplicationController
   def show
     @clan = current_user.clan
     @members_by_position = @clan.users.order(score: :desc)
+    @score = @clan.users.sum(&:score)
   end
 end
