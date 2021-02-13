3862ca354f2fbc999e2301aaff47bd84096bb32c
Audrey de Marco <demarco.audrey@gmail.com>
2021-02-13 17:17:30 +0100

    add a new member to my clan

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :challenges, only: [ :show ]
   resources :users, only: [ :show ]
-  resources :clans, only: [ :show ]
+  resources :clans, only: [ :show ] do
+    post :add_user, on: :member
+  end
   resources :user_challenges, only: [ :index, :create, :show] do
     member { patch :accept, :decline, :validate, :drop }
   end
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
