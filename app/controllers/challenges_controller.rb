f68b6db4a34674a105eeb4a6751ccae347043e77
Myriam Faucher Pelletier <myriamfpelletier@gmail.com>
2021-02-14 16:03:52 +0100

    disabled notif when own challenge

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,5 +1,6 @@
 class ChallengesController < ApplicationController
   def show
+    @disable_bell = true
     @challenge = Challenge.find(params[:id])
     @user_challenge = current_user.user_challenges.find_by(challenge: @challenge)
   end
