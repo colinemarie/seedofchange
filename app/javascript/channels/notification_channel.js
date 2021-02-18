d97a50a2959b3f67544b1b312e98ea1ead651d54
Coline Juillerat <coline.ju@gmail.com>
2021-02-18 20:21:26 +0100

    notif debug

--- app/javascript/channels/notification_channel.js
+++ app/javascript/channels/notification_channel.js
@@ -4,9 +4,14 @@ const initNotificationCable = () => {
   const notif = document.getElementById('notif');
   if (notif) {
     const id = notif.dataset.clanId;
+    const currentUser = notif.dataset.currentUser;
     consumer.subscriptions.create({ channel: "NotificationChannel", id: id }, {
       received(data) {
-        notif.innerHTML = data
+      const html = $(data);
+      const user = html[0].dataset.user === currentUser;
+        if (!user) {
+          notif.innerHTML = data
+        }
       },
     });
   }
