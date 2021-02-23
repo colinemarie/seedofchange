c63694073825ac285f9562b1b045a5edba2adbe2
Myriam Faucher Pelletier <myriamfpelletier@gmail.com>
2021-02-23 21:25:37 +0100

    debut de sons

---
+++ app/javascript/components/notifSound.js
@@ -0,0 +1,9 @@
+const validateButton = document.getElementById("validate-btn");
+const audio = new Audio('validate.mp3');
+
+const notifSound = () => {
+  validateButton.addEventListener("click", (event) => {
+    audio.play();
+  });
+}
+export {notifSound};
