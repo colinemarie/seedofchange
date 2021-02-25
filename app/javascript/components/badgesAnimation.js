3c716e6d6ece555d3014e30afe1d4379318a4667
Simon Dautun <dautun.simon@gmail.com>
2021-02-25 21:03:46 +0100

    style in the code

--- app/javascript/components/badgesAnimation.js
+++ app/javascript/components/badgesAnimation.js
@@ -1,35 +1,37 @@
-const cardsChallenge = document.querySelectorAll(".card-challenge");
 
 
 const animateBadges =() => {
   if (document.querySelector("#users-show")) {
 
+    const cardsChallenge = document.querySelectorAll(".card-challenge");
 
-  cardsChallenge.forEach((challenge) => {
-    const challengeProgress = challenge.querySelector(".progression-bar > progress").value;
-    const challengeBadges = challenge.querySelectorAll('[id^="badges_"]');
-    const challengeLevel = challenge.querySelector('.badges').dataset.level;
+    if (!cardsChallenge) return;
 
-    switch (challengeLevel) {
-      case "2":
-        challengeBadges[0].classList.add("badge-gold");
-      break;
-      case "3":
-        challengeBadges[0].classList.add("badge-gold");
-        challengeBadges[1].classList.add("badge-gold");
-        console.log('level 3');
-      break;
-      case "4":
-        challengeBadges[0].classList.add("badge-gold");
-        challengeBadges[1].classList.add("badge-gold");
-        challengeBadges[2].classList.add("badge-gold");
-        console.log('level 4');
-      break;
+    cardsChallenge.forEach((challenge) => {
+      const challengeProgress = challenge.querySelector(".progression-bar > progress").value;
+      const challengeBadges = challenge.querySelectorAll('[id^="badges_"]');
+      const challengeLevel = challenge.querySelector('.badges').dataset.level;
 
-    }
+      switch (challengeLevel) {
+        case "2":
+          challengeBadges[0].classList.add("badge-gold");
+        break;
+        case "3":
+          challengeBadges[0].classList.add("badge-gold");
+          challengeBadges[1].classList.add("badge-gold");
+          console.log('level 3');
+        break;
+        case "4":
+          challengeBadges[0].classList.add("badge-gold");
+          challengeBadges[1].classList.add("badge-gold");
+          challengeBadges[2].classList.add("badge-gold");
+          console.log('level 4');
+        break;
+
+      }
 
 
-  })
+    })
 
   }
 
--- app/javascript/components/initRead.js
+++ app/javascript/components/initRead.js
@@ -1,38 +1,40 @@
-const initRead = () => {
-  const activity = document.querySelector(".activities");
-  if (activity) {
-    setTimeout(() => {
-      postAsRead(activity)
-      markAsRead(activity)
-    }, 2500);
+
+
+  const initRead = () => {
+    if (document.querySelector(".activities")) {
+    const activity = document.querySelector(".activities");
+      setTimeout(() => {
+        postAsRead(activity)
+        markAsRead(activity)
+      }, 2500);
+    }
   }
-}
 
-const postAsRead = (activity) => {
-  const id = activity.dataset.userId
-  const url = activity.dataset.baseUrl + "activities/read"
-  const token = activity.dataset.authToken
-  fetch(url, {
-    method: "POST",
-    headers: {
-    'X-Requested-With': 'XMLHttpRequest',
-    'X-CSRF-Token': token,
-    'Content-Type': 'application/json',
-            'Accept': 'application/json'
-    },
-    credentials: 'same-origin',
-    body: JSON.stringify({ id: id })
-  })
-}
+  const postAsRead = (activity) => {
+    const id = activity.dataset.userId
+    const url = activity.dataset.baseUrl + "activities/read"
+    const token = activity.dataset.authToken
+    fetch(url, {
+      method: "POST",
+      headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+      'X-CSRF-Token': token,
+      'Content-Type': 'application/json',
+              'Accept': 'application/json'
+      },
+      credentials: 'same-origin',
+      body: JSON.stringify({ id: id })
+    })
+  }
 
-const markAsRead = (activity) => {
-  const unreads = document.querySelectorAll(".unread");
-  const circle = document.querySelector(".fa-circle");
-  unreads.forEach((unread) => {
-    unread.classList.remove("unread")
-  });
-  circle.classList.remove("fa-circle");
-}
+  const markAsRead = (activity) => {
+    const unreads = document.querySelectorAll(".unread");
+    const circle = document.querySelector(".fa-circle");
+    unreads.forEach((unread) => {
+      unread.classList.remove("unread")
+    });
+    circle.classList.remove("fa-circle");
+  }
 
 
 
