b8278203a18cde9ee1f79ea6825dc42ef2ff2185
Coline Juillerat <coline.ju@gmail.com>
2021-03-12 20:53:10 +0100

    add first_name, username and photo to sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :user_challenges, dependent: :destroy
   has_many :challenges, through: :user_challenges
   has_many :activities, dependent: :destroy
   has_many :invitations, dependent: :destroy
   belongs_to :clan, optional: true
   has_one_attached :photo
+  validates :first_name, :username, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 end
